17c8dd121744be060eb6b06a11769610bec31059
wembye <135704212+wembye@users.noreply.github.com>
2026-06-27 21:07:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ.get("8716311827:AAHemWBB9X8srTr8pqmaHz4Zhj1F_T7exVA")
 WEB_APP_URL = "https://project-nv8gi.vercel.app/"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):

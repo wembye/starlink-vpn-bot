9da27a8b39deee3d2f3090b228c2b4da386ff792
wembye <135704212+wembye@users.noreply.github.com>
2026-06-28 20:57:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandle
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = "8716311827:AAHemWBB9X8srTr8pqmaHz4Zhj1F_T7exVA"
 WEB_APP_URL = os.environ.get("WEB_APP_URL", "https://project-nv8gi.vercel.app/")
 
 # Stars prices (1 Star = ~0.013 USD, примерно)
